015fa9bbb5cb70319e0cabe22629aadd79f59946
paulocpd76 <37601162+paulocpd76@users.noreply.github.com>
2018-04-12 21:19:08 +0200

    first

--- app/src/main/java/pperez003/example/com/myhelp/Tab1Register.java
+++ app/src/main/java/pperez003/example/com/myhelp/Tab1Register.java
@@ -5,6 +5,7 @@ package pperez003.example.com.myhelp;
  */
 
 
+import android.os.Build;
 import android.support.v4.app.Fragment;
         import android.os.Bundle;
         import android.view.LayoutInflater;
@@ -14,26 +15,45 @@ import android.support.v4.app.Fragment;
         import org.altbeacon.beacon.Beacon;
         import org.altbeacon.beacon.BeaconParser;
         import org.altbeacon.beacon.BeaconTransmitter;
+        import android.bluetooth.BluetoothAdapter;
+import android.widget.TextView;
 
 public class Tab1Register extends  Fragment {
 
-    @Override
+
+    private BluetoothAdapter blVer;
+    private TextView device;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab1reg, container, false);
+        device = rootView.findViewById(R.id.device);
+        blVer = BluetoothAdapter.getDefaultAdapter();
+        boolean le2MPhySupported;
+        //device.setText("hola");
+        if (Build.VERSION.SDK_INT >= 21) {
+            // Call some material design APIs here
+            device.setText("supported");
+            Beacon beacon = new Beacon.Builder()
+                    .setId1("2f234454-cf6d-4a0f-adf2-f4911ba9ffa6")
+                    .setId2("1")
+                    .setId3("2")
+                    .setManufacturer(0x0118)
+                    .setTxPower(-59)
+                    .setDataFields(Arrays.asList(new Long[] {0l}))
+                    .build();
+            BeaconParser beaconParser = new BeaconParser()
+                    .setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25");
+            BeaconTransmitter beaconTransmitter = new BeaconTransmitter(getActivity(), beaconParser);
+            beaconTransmitter.startAdvertising(beacon);
+        } else {
+            // Implement this feature without material design
+            device.setText("Not supported");
+        }
 
-        Beacon beacon = new Beacon.Builder()
-                .setId1("2f234454-cf6d-4a0f-adf2-f4911ba9ffa6")
-                .setId2("1")
-                .setId3("2")
-                .setManufacturer(0x0118)
-                .setTxPower(-59)
-                .setDataFields(Arrays.asList(new Long[] {0l}))
-                .build();
-        BeaconParser beaconParser = new BeaconParser()
-                .setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25");
-        BeaconTransmitter beaconTransmitter = new BeaconTransmitter(getActivity(), beaconParser);
-        beaconTransmitter.startAdvertising(beacon);
+
+
+
+
         return rootView;
     }
 }
